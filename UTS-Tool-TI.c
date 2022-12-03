e5d9a4277bd14345aa6a06d8adb673933c52f352
DwitabPutri <putrisuastini22@gmail.com>
2022-12-04 07:04:08 +0800

    Menambahkan nim dan mata kuliah

--- UTS-Tool-TI.c
+++ UTS-Tool-TI.c
@@ -4,4 +4,7 @@ int main(){
     printf("UTS Tool TI");
     //menambahkan nama
     printf("\nNama : Ni Kadek Dwita Putri Suastini");
+    //menambahkan nim dan mata kuliah
+    printf("\nNIM : 2205551074");
+    printf("\nMata Kuliah : Tool TI");
 }
